03427a66d6ee71746832608620d07f234b0f2139
divakar <divakarsathees7@gmail.com>
2023-04-12 09:14:48 +0000

    initial Setup

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,118 @@
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EventApi.Controllers;
+using EventApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventApi.Tests
+{
+    public class EventApiControllerTests
+    {
+        private EventApiDbContext _context;
+        private EventApiController _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            var options = new DbContextOptionsBuilder<EventApiDbContext>()
+                .UseInMemoryDatabase(databaseName: "EventApiTest")
+                .Options;
+
+            _context = new EventApiDbContext(options);
+
+            _controller = new EventApiController(_context);
+
+            _context.EventApis.AddRange(new List<EventApi.Models.EventApi>
+            {
+                new EventApi.Models.EventApi { Id = 1, Event_Name = "Event 1", Event_Type = "Type A", Start_Date = new DateTime(2023, 4, 20), End_Date = new DateTime(2023, 4, 25), Location = "Location A" },
+                new EventApi.Models.EventApi { Id = 2, Event_Name = "Event 2", Event_Type = "Type B", Start_Date = new DateTime(2023, 5, 1), End_Date = new DateTime(2023, 5, 5), Location = "Location B" },
+                new EventApi.Models.EventApi { Id = 3, Event_Name = "Event 3", Event_Type = "Type C", Start_Date = new DateTime(2023, 6, 1), End_Date = new DateTime(2023, 6, 5), Location = "Location C" }
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+        }
+
+        [Test]
+        public async Task GetEventApis_ReturnsAllEvents()
+        {
+            var result = await _controller.GetEventApis();
+
+            Assert.IsInstanceOf(typeof(List<EventApi.Models.EventApi>), result.Value);
+            Assert.AreEqual(3, result.Value.Count());
+        }
+
+    
+        [Test]
+        public async Task GetEventApi_InvalidId_ReturnsNotFound()
+        {
+            int eventId = 100;
+
+            var result = await _controller.GetEventApi(eventId);
+
+            Assert.IsInstanceOf(typeof(NotFoundResult), result.Result);
+            
+        }
+
+        [Test]
+        public async Task GetEventApi_ReturnsSingleEvent()
+        {
+            int eventId = 2;
+
+            var result = await _controller.GetEventApi(eventId);
+
+            Assert.IsInstanceOf(typeof(EventApi.Models.EventApi), result.Value);
+            Assert.AreEqual(eventId, result.Value.Id);
+        }
+
+        [Test]
+        public async Task DeleteEventApi_DeletesExistingEvent()
+        {
+            int eventId = 1;
+
+            var result = await _controller.DeleteEventApi(eventId);
+
+            Assert.IsInstanceOf(typeof(NoContentResult), result);
+            var eventFromDb = _context.EventApis.Find(eventId);
+            Assert.IsNull(eventFromDb);
+        }
+
+         [Test]
+        public async Task PostEventApi_WithValidData_ReturnsCreatedAtActionResult()
+        {
+            var testEventApi = new EventApi.Models.EventApi {Id = 4 ,Event_Name = "Event 4", Event_Type = "Type D", Start_Date = new DateTime(2023, 7, 1), End_Date = new DateTime(2023, 7, 5), Location = "Location D" };
+
+            var result = await _controller.PostEventApi(testEventApi);
+
+            Assert.IsInstanceOf<CreatedAtActionResult>(result.Result);
+        }
+
+        [Test]
+        public async Task PostEventApi_WithValidData_ReturnsCreatedEventApi()
+        {
+            var testEventApi = new EventApi.Models.EventApi { Id = 5 ,Event_Name = "Event 5", Event_Type = "Type D", Start_Date = new DateTime(2023, 7, 1), End_Date = new DateTime(2023, 7, 5), Location = "Location D"  };
+
+            var result = await _controller.PostEventApi(testEventApi);
+
+            var createdResult = result.Result as CreatedAtActionResult;
+            Assert.IsNotNull(createdResult);
+
+            var createdEventApi = createdResult.Value as EventApi.Models.EventApi;
+            Assert.IsNotNull(createdEventApi);
+            Assert.AreEqual(testEventApi.Event_Name, createdEventApi.Event_Name);
+            Assert.AreEqual(testEventApi.Event_Type, createdEventApi.Event_Type);
+            Assert.AreEqual(testEventApi.Start_Date, createdEventApi.Start_Date);
+            Assert.AreEqual(testEventApi.End_Date, createdEventApi.End_Date);
+        }        
+    }
+}
